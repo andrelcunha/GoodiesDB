8709a327420ebf1a9ec00859af61a3bd1804f492
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-11-21 02:19:01 -0300

    Add utility functions for slice operations
    - Created internal/utils/slice package
    - Added Equal and Reverse functions for comparing and reversing slices

---
+++ internal/utils/slice/slice.go
@@ -0,0 +1,20 @@
+package slice
+
+// Helper function to compare slices
+func Equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func Reverse(slice []string) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
